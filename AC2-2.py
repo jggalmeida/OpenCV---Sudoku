84c8846b6ac0d95a813ac3c26cca4b6ad2a23adb
Eric Monne Mesalles <emonne97@gmail.com>
2020-05-21 16:56:52 +0200

    final

--- AC2-2.py
+++ AC2-2.py
@@ -1,59 +1,43 @@
-# Standard imports
 import cv2
-import glob
+from matplotlib import pyplot as plt
 import numpy as np
+import glob
 
-font = cv2.FONT_HERSHEY_SIMPLEX
+# Eric Monné Mesalles - 183338
+# João Guilherme Guimarães Almeida - 120693
 
-path = './data/*'
-imagesPath = [img for img in glob.glob(path)]
-# print( glob.glob(path)[0] in "jpg")
-print(imagesPath)
-images = []
-images = list(map(lambda x: cv2.imread(x, cv2.IMREAD_GRAYSCALE),imagesPath))
+images = glob.glob('./data/*')
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+images = list(map(lambda x :cv2.imread(x,cv2.IMREAD_COLOR), images))
 
+grey = list(map(lambda x : cv2.cvtColor(x, cv2.COLOR_BGR2GRAY), images))
 
-kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
-noSpaceKernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(4,4))
-
-
-images = list(map(lambda x:cv2.erode(x,noSpaceKernel),images))
-# opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+thresh = list(map(lambda x: cv2.adaptiveThreshold(x,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,15,4),grey))
+thresh = list(map(lambda x: cv2.morphologyEx(x, cv2.MORPH_CLOSE, kernel),thresh))
 
 
-# gray_image = cv2.cvtColor(opening, cv2.COLOR_BGR2GRAY)
-#
-#
-#
-#
-# #binary image
-# _,thresh = cv2.threshold(gray_image,71,255,cv2.THRESH_BINARY_INV)
-#
-#
-#
-#
-# #get contours
-# contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-# height, width = gray_image.shape
-# min_x, min_y  = width, height
-# max_x = max_y = 0
-#
-#
-# # computes the bounding box for the contour', and draws it on the frame,
-# for contour in contours :
-#     (x,y,w,h) = cv2.boundingRect(contour)
-#     min_x, max_x = min(x, min_x), max(x+w, max_x)
-#     min_y, max_y = min(y, min_y), max(y+h, max_y)
-#     if w >= 8 and h >= 20 and w < 30 and h < 30:
-#         cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 1)
-#         print("x = ", x , " y= ", y , " w = ", w , " h = ", h )
-#
-#
-#
-# cv2.imshow("Thresh ", thresh)
-for img in images:
-    cv2.imshow("Blobs ",  img)
 
+for i in range(len(thresh)):
+    contours, hierarchy = cv2.findContours(thresh[i], cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    height, width = grey[i].shape
+    min_x, min_y  = width, height
+    max_x = max_y = 0
+    numbers = 0
+    # computes the bounding box for the contour, and draws it on the frame,
+    for contour in contours :
+        (x,y,w,h) = cv2.boundingRect(contour)
+        if (len(contours) > 190):
+            if ( h < height / 23):
+                numbers += 1
+                min_x, max_x = min(x, min_x), max(x+w, max_x)
+                min_y, max_y = min(y, min_y), max(y+h, max_y)
+                cv2.rectangle(images[i], (-2+x,-2+y), (x+w+2,y+h+2), (255,0,0), 1)
+        else:
+            if ( h < height / 11 and h > height / 25):
+                numbers += 1
+                min_x, max_x = min(x, min_x), max(x+w, max_x)
+                min_y, max_y = min(y, min_y), max(y+h, max_y)
+                cv2.rectangle(images[i], (-2+x,-2+y), (x+w+2,y+h+2), (255,0,0), 1)
+    cv2.imshow("Numbers =" + str(numbers),images[i])
 
 cv2.waitKey(0)
